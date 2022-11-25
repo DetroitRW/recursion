e2b4bad0b8a2237423b0fc6e0003f0769831ce3f
Ilya Brant <detroitib31@gmail.com>
2022-11-25 12:59:02 +0600

    Написал функцию и алгоритмы в ней

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,4 +3,19 @@ import java.util.Scanner
 fun main(args: Array<String>) {
     val reader = Scanner(System.`in`)
 
+    print("Введите цисло: ")
+
+    fun recursion () {
+        val n = reader.nextInt()
+
+        var f = 1
+        if (n > 0) {
+            f = 1
+        }
+
+        for (i in (1..n)) {
+            f *= i
+        }
+        println("Факториал числа: $n = $f")
+    }
 }
